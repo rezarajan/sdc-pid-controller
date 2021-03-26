ca0720418011824d185de55c5beefbac324d20a2
rezarajan <rezarajan@gmail.com>
2021-03-26 10:00:39 -0400

    feat: PID class setup

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,21 +10,32 @@ PID::~PID() {}
 
 void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
+   * ~TODO~: Initialize PID coefficients (and errors, if needed)
    */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+
+  p_error = 0, i_error = 0, d_error = 0;
 
 }
 
 void PID::UpdateError(double cte) {
   /**
-   * TODO: Update PID errors based on cte.
+   * ~TODO~: Update PID errors based on cte.
    */
+  p_error = Kp * cte;
+  i_error += Ki * cte;
+  d_error = Kd * (cte - d_error);
+  /**
+   * TODO: create time variable for timestep in updating the differential error
+   */ 
 
 }
 
 double PID::TotalError() {
   /**
-   * TODO: Calculate and return the total error
+   * ~TODO~: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  return p_error + i_error + d_error;  // TODO: Add your total error calc here!
 }
